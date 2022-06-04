e62a3beb369d065c8edbfdae92b8ff7823c01a9d
Artem Lipinskiy <artem_lipinskiy@mail.ru>
2022-06-04 10:13:24 +0200

    Add project files.

---
+++ MontenegroRent.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MontenegroRent.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Info")]
+        public async Task<string> Get()
+        {
+            return $"MontenegroRent.WebApi is running {DateTime.UtcNow}";
+        }
+    }
+}
